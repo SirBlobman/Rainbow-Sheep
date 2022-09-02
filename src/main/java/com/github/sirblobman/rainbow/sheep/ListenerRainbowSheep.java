ac2868f9b73f7cbbe060fbd1c104b38522fdcde5
SirBlobman <blobmansrealm@gmail.com>
2022-09-02 11:26:02 -0400

    1.0.4: Update to BlueSlimeCore 2.5-SNAPSHOT.

--- src/main/java/com/github/sirblobman/rainbow/sheep/ListenerRainbowSheep.java
+++ src/main/java/com/github/sirblobman/rainbow/sheep/ListenerRainbowSheep.java
@@ -1,31 +1,20 @@
 package com.github.sirblobman.rainbow.sheep;
 
-import java.util.Objects;
-
-import org.bukkit.Bukkit;
 import org.bukkit.Chunk;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Sheep;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
-import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.entity.EntitySpawnEvent;
 import org.bukkit.event.world.ChunkLoadEvent;
-import org.bukkit.plugin.PluginManager;
 
 import com.github.sirblobman.api.nms.EntityHandler;
+import com.github.sirblobman.api.plugin.listener.PluginListener;
 
-public final class ListenerRainbowSheep implements Listener {
-    private final RainbowSheepPlugin plugin;
-
+public final class ListenerRainbowSheep extends PluginListener<RainbowSheepPlugin> {
     public ListenerRainbowSheep(RainbowSheepPlugin plugin) {
-        this.plugin = Objects.requireNonNull(plugin, "plugin must not be null!");
-    }
-
-    public void register() {
-        PluginManager pluginManager = Bukkit.getPluginManager();
-        pluginManager.registerEvents(this, this.plugin);
+        super(plugin);
     }
 
     @EventHandler(priority=EventPriority.MONITOR, ignoreCancelled=true)
@@ -61,7 +50,8 @@ public final class ListenerRainbowSheep implements Listener {
             AdventureHelper.setCustomName(sheep, "jeb_", false);
         } catch(ClassCastException | NoClassDefFoundError ex) {
             try {
-                EntityHandler entityHandler = this.plugin.getEntityHandler();
+                RainbowSheepPlugin plugin = getPlugin();
+                EntityHandler entityHandler = plugin.getEntityHandler();
                 entityHandler.setCustomNameTextOnly(sheep, "jeb_", false);
             } catch(Exception ex2) {
                 sheep.setCustomName("jeb_");
--- src/main/java/com/github/sirblobman/rainbow/sheep/RainbowSheepPlugin.java
+++ src/main/java/com/github/sirblobman/rainbow/sheep/RainbowSheepPlugin.java
@@ -2,23 +2,41 @@ package com.github.sirblobman.rainbow.sheep;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.github.sirblobman.api.core.CorePlugin;
 import com.github.sirblobman.api.nms.EntityHandler;
 import com.github.sirblobman.api.nms.MultiVersionHandler;
+import com.github.sirblobman.api.plugin.ConfigurablePlugin;
+import com.github.sirblobman.api.update.UpdateManager;
 
-public final class RainbowSheepPlugin extends JavaPlugin {
-    private final EntityHandler entityHandler;
-
-    public RainbowSheepPlugin() {
-        MultiVersionHandler multiVersionHandler = new MultiVersionHandler(this);
-        this.entityHandler = multiVersionHandler.getEntityHandler();
+public final class RainbowSheepPlugin extends ConfigurablePlugin {
+    @Override
+    public void onLoad() {
+        // Empty Method
     }
 
     @Override
     public void onEnable() {
-        new ListenerRainbowSheep(this).register();
+        registerListeners();
+        registerUpdateChecker();
+    }
+
+    @Override
+    public void onDisable() {
+        // Empty Method
     }
 
     public EntityHandler getEntityHandler() {
-        return this.entityHandler;
+        MultiVersionHandler multiVersionHandler = getMultiVersionHandler();
+        return multiVersionHandler.getEntityHandler();
+    }
+
+    private void registerListeners() {
+        new ListenerRainbowSheep(this).register();
+    }
+
+    private void registerUpdateChecker() {
+        CorePlugin corePlugin = JavaPlugin.getPlugin(CorePlugin.class);
+        UpdateManager updateManager = corePlugin.getUpdateManager();
+        updateManager.addResource(this, 77075L);
     }
 }
